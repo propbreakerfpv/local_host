bae58af5d1257a1e572e4d7c1374dccb5f5c4440
Jonas Breen <jonasebreen@gmail.com>
2024-02-29 23:39:24 -0500

    clap argument parseing, suport for specifying path, specifying multiple paths

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,50 @@
-use std::{fs, env::args};
+use std::fs;
 
-use axum::{Router, routing::get, extract::State, response::Html};
+use axum::{
+    extract::{Path, State},
+    response::Html,
+    routing::get,
+    Router,
+};
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(version)]
+struct Arguments {
+    #[arg(short, long)]
+    rout: Vec<String>,
+    #[arg(short, long, default_value = "1337")]
+    port: u16,
+}
 
 #[derive(Clone)]
 struct AppState {
-    path: String,
+    paths: Vec<String>,
 }
 
 #[tokio::main]
 async fn main() {
-    let path = args().skip(1).fold(String::new(), |mut acc, s| {
-        acc.push_str(&s);
-        acc
-    });
+    let args = Arguments::parse();
 
     let state = AppState {
-        path,
+        paths: args.rout.clone(),
     };
 
-    let app = Router::new()
-        .route("/", get(page))
-        .with_state(state);
+    let app = Router::new().route("/*key", get(page)).with_state(state);
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:1337").await.unwrap();
-    println!("running on 127.0.0.1:1337");
+    let address = format!("127.0.0.1:{}", args.port);
+    let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
+    println!("running on {}", address);
     axum::serve(listener, app).await.unwrap();
 }
 
-
-async fn page(State(state): State<AppState>) -> Html<String> {
-    let content = fs::read_to_string(state.path.clone())
-        .unwrap_or(String::from(format!("file {} not found", state.path)));
-    return Html(content);
+async fn page(Path(path): Path<String>, State(state): State<AppState>) -> Html<String> {
+    for rout in state.paths {
+        if rout.split('/').last() == Some(&path) {
+            let content = fs::read_to_string(rout.clone())
+                .unwrap_or(format!("file {} not found", rout));
+            return Html(content);
+        }
+    }
+    Html(format!("{} not found", path))
 }
