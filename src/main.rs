882cb49a2435170ebc4205f0161f45cf24e4cd0c
jonas breen <jonasebreen@gmail.com>
2024-03-01 19:32:28 -0500

    specify paths to files and routs seperetly

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,16 @@ use clap::Parser;
 struct Arguments {
     #[arg(short, long)]
     rout: Vec<String>,
+    #[arg(short, long)]
+    file: Vec<String>,
     #[arg(short, long, default_value = "1337")]
     port: u16,
 }
 
 #[derive(Clone)]
 struct AppState {
-    paths: Vec<String>,
+    files: Vec<String>,
+    routs: Vec<String>,
 }
 
 #[tokio::main]
@@ -27,7 +30,8 @@ async fn main() {
     let args = Arguments::parse();
 
     let state = AppState {
-        paths: args.rout.clone(),
+        files: args.file.clone(),
+        routs: args.rout.clone(),
     };
 
     let app = Router::new().route("/*key", get(page)).with_state(state);
@@ -39,12 +43,16 @@ async fn main() {
 }
 
 async fn page(Path(path): Path<String>, State(state): State<AppState>) -> Html<String> {
-    for rout in state.paths {
+    print!("got {} ", path);
+    for (idx, rout) in state.routs.iter().enumerate() {
+        let file = state.files.get(idx).unwrap();
+        println!("file: {}", file);
         if rout.split('/').last() == Some(&path) {
-            let content = fs::read_to_string(rout.clone())
-                .unwrap_or(format!("file {} not found", rout));
+            let content = fs::read_to_string(file).unwrap_or(format!("file {} not found", rout));
+            println!("found file {}", file);
             return Html(content);
         }
     }
+    println!("file not found");
     Html(format!("{} not found", path))
 }
